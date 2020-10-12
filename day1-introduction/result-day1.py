7c6f3e61bbbcb4f7dbed489dc9eeb246cb5dd27a
quanganh1996111 <39324832+quanganh1996111@users.noreply.github.com>
2020-10-12 21:59:34 +0700

    yes

---
+++ day1-introduction/result-day1.py
@@ -0,0 +1,26 @@
+# Thực hiện các phép toán
+print('Math:')
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+# Thực hiện in thông tin
+print('Personal Information:')
+print('- Name: Quang Anh Tran')
+print('- Country: Viet Name')
+print('- Age: 24')
+
+# Kiểm tra kiểu dữ liệu
+print('Check Data Type:')
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('HaiDD'))
+print(type('Việt Nam'))
+print(type('24 tuổi'))
